1dab900a3e88c38963f9ae5e45de96ae1fd8fbfa
Sereny Sanchez <52845840+SerenyS@users.noreply.github.com>
2020-09-01 18:57:44 -0500

    Corrected toast product and code display

--- wwwroot/client.js
+++ wwwroot/client.js
@@ -1,13 +1,18 @@
-﻿$(function () {
+﻿
+$(function () {
+   
+
     $('.code').on('click', function (e) {
         e.preventDefault();
+     
+        $('#product').html($(this).data('product'));
+        $('#code').html($(this).data('code'));
         $('#toast').toast({ autohide: false }).toast('show');
     });
-});
 
-$(document).on('keyup', function (e) {
-    if (e.key === "Escape") {
-        $('#toast').toast('hide');
-    }
+    $(document).on('keyup', function (e) {
+        if (e.key === "Escape") {
+            $('#toast').toast('hide');
+        }
+    });
 });
-
