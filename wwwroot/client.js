dfc830161b07308d17b622695e9efe13b918e46e
Sereny Sanchez <52845840+SerenyS@users.noreply.github.com>
2020-09-01 17:51:10 -0500

    Assignment 2 Completed

---
+++ wwwroot/client.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $('.code').on('click', function (e) {
+        e.preventDefault();
+        $('#toast').toast({ autohide: false }).toast('show');
+    });
+});
+
+$(document).on('keyup', function (e) {
+    if (e.key === "Escape") {
+        $('#toast').toast('hide');
+    }
+});
+
